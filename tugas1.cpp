93b7ab42c47fea0e018d7be262be9858ff242276
Malika <malikaayu888@gmail.com>
2026-09-18 23:06:11 +0700

    Add tugas1

---
+++ tugas1.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N, K;
+    cout<<"Input N: ";
+    cin >> N;
+    cout<<"Input K: ";
+    cin >> K;
+
+    int* next_ = new int[N + 1];
+    for (int i = 1; i <= N; i++) {
+        next_[i] = (i == N) ? 1 : i + 1;
+    }
+
+    int current   = 1;
+    int prev       = N;
+    int remaining  = N;
+
+    cout << "Elimination order: ";
+
+    while (remaining > 1) {
+        for (int step = 1; step < K; step++) {
+            prev    = current;
+            current = next_[current];
+        }
+
+        int eliminated = current;
+        cout << eliminated;
+        remaining--;
+        if (remaining > 1) cout << " ";
+
+        next_[prev] = next_[eliminated];
+        current = next_[prev];
+
+        if (eliminated % 2 == 0) {
+            K += 2;
+        } else {
+            K -= 1;
+        }
+        if (K < 2) K = 2;
+    }
+
+    cout << "\nLast surviving astronaut: " << current << endl;
+
+    delete[] next_;
+    return 0;
+}
